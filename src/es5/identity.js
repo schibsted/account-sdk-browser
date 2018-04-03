7f5cdf125a9878d001d18e437db4142767225673
Tor Arvid Lund <tor.arvid.lund@schibsted.com>
2018-04-03 16:09:21 +0200

    Switch parcel with webpack 4 and add es5 targets
    This commit adds compatibility targets for clients who either are unable
    to transpile themselves, or don’t even have a deployment pipeline (iow,
    they need a way to simply include a JS file in an HTML <script> tag).
    
    So now, we’re moving to a way where you can include /es5, or potentially
    /es5/identity etc. if you want a pre-transpiled library.
    
    For now, we keep the babel config that includes the browserslist entries
    "last 2 major versions" and “ie 9”.

---
+++ src/es5/identity.js
@@ -0,0 +1,9 @@
+/* Copyright 2018 Schibsted Products & Technology AS. Licensed under the terms of the MIT license.
+ * See LICENSE.md in the project root.
+ */
+
+'use strict';
+
+require('regenerator-runtime/runtime');
+
+module.exports = require('../identity');
---
+++ src/es5/index.js
@@ -0,0 +1,13 @@
+/* Copyright 2018 Schibsted Products & Technology AS. Licensed under the terms of the MIT license.
+ * See LICENSE.md in the project root.
+ */
+
+'use strict';
+
+require('regenerator-runtime/runtime');
+
+const Identity = require('../../identity');
+const Monetization = require('../../monetization');
+const Payment = require('../../payment');
+
+module.exports = { Identity, Monetization, Payment };
