9b7dea27e88e6710d8e3cdc90dbeaaf0bd8209a7
Tor Arvid Lund <tor.arvid.lund@schibsted.com>
2018-04-03 18:18:54 +0200

    webpack: Fix build options
    The options were not set for output-library-target. They need this so
    that the output is either placed in a ‘module.exports = <blah>’ or for
    the global.js one, ‘window.Blah = blah’.

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-OPTS="--mode=production --module-bind js=babel-loader --devtool source-map"
+COMMON_OPTS="--mode=production --module-bind js=babel-loader --devtool source-map"
+CJS_OPTS="--output-library-target=commonjs2"
+WINDOW_OPTS="--output-library-target=window"
 
 rm -rf es5
-webpack src/es5/index.js -o es5/index.js $OPTS
-webpack src/es5/identity.js -o es5/identity.js $OPTS
-webpack src/es5/monetization.js -o es5/monetization.js $OPTS
-webpack src/es5/payment.js -o es5/payment.js $OPTS
-webpack src/es5/global.js -o es5/global.js $OPTS
+webpack src/es5/index.js -o es5/index.js $COMMON_OPTS $CJS_OPTS
+webpack src/es5/identity.js -o es5/identity.js $COMMON_OPTS $CJS_OPTS
+webpack src/es5/monetization.js -o es5/monetization.js $COMMON_OPTS $CJS_OPTS
+webpack src/es5/payment.js -o es5/payment.js $COMMON_OPTS $CJS_OPTS
+webpack src/es5/global.js -o es5/global.js $COMMON_OPTS $WINDOW_OPTS
