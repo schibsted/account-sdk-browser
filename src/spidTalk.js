4933e60b34ef69db7801823ef2b64523f657a991
Tor Arvid Lund <tor.arvid.lund@schibsted.com>
2018-06-25 15:51:38 +0200

    spidTalk: Fix strange documentation comment

--- src/spidTalk.js
+++ src/spidTalk.js
@@ -17,15 +17,13 @@ import { isFunction } from './validate';
  *
  * How does it work?
  * =================
- * The old SPiD JSONP APIs relied on SPiD.Talk.response(callbackName, data) but fetch-jsonp
- * merely calls window[callbackName](data) which is more common practice. This module simply acts
- * as an adapter to modernize the old mechanism until we support CORS.
- * To see how the SPiD solution works, try to run a simple JSONP request or debug it in netowrk tab.
- * This module will be removed when CORS is supported.
+ * The old SPiD JSONP APIs relied on SPiD.Talk.response(callbackName, data) but fetch-jsonp merely
+ * calls window[callbackName](data) which is more common practice. This module simply acts as an
+ * adapter to modernize the old mechanism until we support CORS. To see how the SPiD solution works,
+ * try to run a simple JSONP request or debug it in netowrk tab. This module will be removed when
+ * CORS is supported.
  * @memberof core
- * @param {object} global - a reference to the global object if SPiD SDK is used in the var mode
- *        you can also pass a reference to SPiD directly in which case the `objectName` param should
- *        be omitted.
+ * @param {object} global - a reference to the global object
  * @return {void}
  */
 export function emulate(global) {
