e7cacf262a83e4b8734e544a513157a8f3e03fad
Martin Pedersen <martin@m4r7.in>
2018-10-11 10:14:18 +0200

    fix typeof in clearVarnishCookie

--- src/identity.js
+++ src/identity.js
@@ -348,7 +348,7 @@ export class Identity extends EventEmitter {
      * @returns {void}
      */
     _clearVarnishCookie() {
-        const domain = (typeof this._session && this._session.baseDomain === 'string')
+        const domain = (this._session && typeof this._session.baseDomain === 'string')
             ? this._session.baseDomain
             : (getTopDomain(document.domain) || '');
         document.cookie = `SP_ID=nothing; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/; domain=.${domain}`;
