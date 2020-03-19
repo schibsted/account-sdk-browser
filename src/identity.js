e37d26eab6fc7497d4e4463924d333d6b05466b7
juszkiewicz <marcin.juszkiewicz@schibsted.pl>
2020-03-19 12:42:52 +0100

    Cache session service response only in case of siteSpecificLogout, otherwise we do fallback to spid. Also- cache only 400 http errors. If user session expire session service returns 401 and removes session cookie. So on next request session service will respond with 400.

--- src/identity.js
+++ src/identity.js
@@ -466,11 +466,11 @@ export class Identity extends EventEmitter {
                 try {
                     sessionData = await this._sessionService.get('/session');
                 } catch (err) {
-                    if (err && this._enableSessionCaching) {
-                        const expiresIn = 1000 * (err.expiresIn || 300);
-                        this.cache.set(HAS_SESSION_CACHE_KEY, { error: err }, expiresIn);
-                    }
                     if (this.siteSpecificLogout) {
+                        if (err && err.code === 400 && this._enableSessionCaching) {
+                            const expiresIn = 1000 * (err.expiresIn || 300);
+                            this.cache.set(HAS_SESSION_CACHE_KEY, { error: err }, expiresIn);
+                        }
                         // Don't fallback to other sources for user session lookup
                         throw err;
                     }
