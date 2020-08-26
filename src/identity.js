cf0b61859ffacc1c0a0a5de3ed2f303cc73938b7
juszkiewicz <marcin.juszkiewicz@schibsted.pl>
2020-08-26 09:43:23 +0200

    2FA supports sms, otp, password values in multiple combinations

--- src/identity.js
+++ src/identity.js
@@ -781,7 +781,8 @@ export class Identity extends EventEmitter {
             teaser = arguments[6] || teaser;
             maxAge = isNaN(arguments[7]) ? maxAge : arguments[7];
         }
-        assert(!acrValues || isStrIn(acrValues, ['', 'otp-email', 'otp-sms', 'mfa', 'otp', 'sms', 'otp sms'], true),
+        const isValidAcrValue = (acrValue) => isStrIn(acrValue, ['password', 'otp', 'sms'], true);
+        assert(!acrValues || isStrIn(acrValues, ['', 'otp-email', 'otp-sms'], true) || acrValues.split(' ').every(isValidAcrValue),
             `The acrValues parameter is not acceptable: ${acrValues}`);
         assert(isUrl(redirectUri),
             `loginUrl(): redirectUri must be a valid url but is ${redirectUri}`);
