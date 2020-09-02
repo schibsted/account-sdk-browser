a53a053b754bacd2ecc896d07e5c1699f5dda96a
Paweł <pawel.chyla@schibsted.com>
2020-09-02 12:42:25 +0200

    Test for logSettings

--- __tests__/identity.js
+++ __tests__/identity.js
@@ -10,6 +10,7 @@ import Identity from '../identity';
 import { compareUrls, Fixtures } from './utils';
 import { URL } from 'url';
 import { URL as u } from 'whatwg-url';
+import {version} from "../package.json";
 
 describe('Identity', () => {
     const defaultOptions = {
@@ -803,4 +804,52 @@ describe('Identity', () => {
             expect(window.openSimplifiedLoginWidget).toHaveBeenCalledTimes(1);
         });
     });
+
+    describe('logSettings', () => {
+        test('should print settings and version', () => {
+            const window = { location: {} };
+            const log = jest.fn();
+            const settings = {
+                clientId: defaultOptions.clientId,
+                redirectUri: defaultOptions.redirectUri,
+                env: 'PRE',
+                sessionDomain: defaultOptions.sessionDomain,
+                sdkVersion: version
+            }
+
+            const identity = new Identity(Object.assign({}, defaultOptions, { window, log }));
+            identity.logSettings();
+
+            expect(log).toHaveBeenCalledWith(`Schibsted account SDK for browsers settings: \n${JSON.stringify(settings, null, 2)}`);
+        })
+
+        test('should use console.log when exist and log do not exist', () => {
+            jest.spyOn(console, 'log').mockImplementation(jest.fn());
+
+            const window = { location: {}};
+            const settings = {
+                clientId: defaultOptions.clientId,
+                redirectUri: defaultOptions.redirectUri,
+                env: 'PRE',
+                sessionDomain: defaultOptions.sessionDomain,
+                sdkVersion: version
+            }
+
+            const identity = new Identity(Object.assign({}, defaultOptions, { window }));
+            identity.logSettings();
+
+            expect(console.log).toHaveBeenCalledWith(`Schibsted account SDK for browsers settings: \n${JSON.stringify(settings, null, 2)}`);
+        })
+
+        test('should throw error when log and console.log do not exist', () => {
+            const window = { location: {} };
+            const identity = new Identity(Object.assign({}, defaultOptions, { window }));
+
+            try {
+                identity.logSettings()
+            } catch (e) {
+                expect(e).toThrowError(new SDKError('You have to provide log method in constructor'));
+            }
+        })
+    })
 });
